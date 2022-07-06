29780493790fc75939ee5d726025a40d2e6f792b
jiaxiao243 <jiaxiao243.com>
2022-07-06 18:20:31 +0800

    'update'

--- run_TestCase.bat
+++ run_TestCase.bat
@@ -1,4 +1,4 @@
-rm -rf PaddleOCR/rec_*
+set sed="C:\Program Files\Git\usr\bin\sed.exe"
+export CUDA_VISIBLE_DEVICES=0
 python -m pip install -r requirements.txt
-export CUDA_VISIBLE_DEVICES=0,1
 python -m  pytest -sv test_ocr_acc.py --html=rec_report.html --capture=tee-sys
