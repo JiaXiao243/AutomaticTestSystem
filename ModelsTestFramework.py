aeb4d85217d11690f9da5ad12774943e7fbfde8e
jiaxiao243 <jiaxiao243@126.com>
2022-07-07 10:50:26 +0000

    add table rec

--- ModelsTestFramework.py
+++ ModelsTestFramework.py
@@ -88,117 +88,31 @@ def exit_check_fucntion(exit_code, output, mode, log_dir=''):
     assert 'ABORT!!!' not in output, "%s  model failed!   log information:%s" % (mode, output)
     logging.info("train model sucessfuly!" )
 
+def  check_infer_metric(category, output):
+     if category=='rec':
+        metric=metricExtraction('result', output)
+        rec_docs=metric.strip().split('\t')[0]
+        rec_scores=metric.strip().split('\t')[1]
+        rec_scores=float(rec_scores)
 
-class TestOcrModelFunction():
-      def __init__(self, model, yml): 
-         self.model=model
-         self.yaml=yml
+        print('rec_docs:{}'.format(rec_docs))
+        print('rec_scores:{}'.format(rec_scores))
 
-         self.testcase_yml=yaml.load(open('TestCase.yaml','rb'), Loader=yaml.Loader)
-         self.tar_name=os.path.splitext(os.path.basename(self.testcase_yml[self.model]['eval_pretrained_model']))[0]
-               
-      def test_ocr_train(self, use_gpu):
-          cmd='cd PaddleOCR; export CUDA_VISIBLE_DEVICES=0; sed -i s!data_lmdb_release/training!data_lmdb_release/validation!g %s; python -m paddle.distributed.launch --log_dir=log_%s  tools/train.py -c %s -o Global.use_gpu=%s Global.epoch_num=1 Global.save_epoch_step=1 Global.eval_batch_step=200 Global.print_batch_step=10 Global.save_model_dir=output/%s Train.loader.batch_size_per_card=10 Global.print_batch_step=1;' % (self.yaml,  self.model, self.yaml, use_gpu, self.model)
-          if(platform.system() == "Windows"):
-               cmd=cmd.replace(';','&')
-               cmd=cmd.replace('sed','%sed%')
-          if(platform.system() == "Darwin"):
-               cmd=cmd.replace('sed -i','sed -i ""')
-          print(cmd)
-          detection_result = subprocess.getstatusoutput(cmd)
-          exit_code = detection_result[0]
-          output = detection_result[1]
-          log_dir='PaddleOCR/log_'+self.model
-          exit_check_fucntion(exit_code, output, 'train', log_dir)
-      
-      def test_ocr_get_pretrained_model(self):
-          cmd='cd PaddleOCR; wget %s; tar xf %s.tar; rm -rf *.tar; mv %s %s;' % (self.testcase_yml[self.model]['eval_pretrained_model'], self.tar_name, self.tar_name, self.model)
-          if(platform.system() == "Windows"):
-               cmd=cmd.replace(';','&')
-               cmd=cmd.replace('rm -rf', 'del')
-               cmd=cmd.replace('mv','ren')
-          print(cmd)
-          detection_result = subprocess.getstatusoutput(cmd)
-          exit_code = detection_result[0]
-          output = detection_result[1]
-          exit_check_fucntion(exit_code, output, 'eval')
+        expect_rec_docs='joint'
+        expect_rec_scores=0.9999
 
-      def test_ocr_eval(self, use_gpu):
-          cmd='cd PaddleOCR; python tools/eval.py -c %s  -o Global.use_gpu=%s Global.pretrained_model=./%s/best_accuracy' % (self.yaml, use_gpu, self.model)
-          if(platform.system() == "Windows"):
-               cmd=cmd.replace(';','&')
-          print(cmd)
-          detection_result = subprocess.getstatusoutput(cmd)
-          exit_code = detection_result[0]
-          output = detection_result[1]
-          exit_check_fucntion(exit_code, output, 'eval')
-          real_metric=metricExtraction('acc', output)
-          expect_metric=self.testcase_yml[self.model]['eval_acc']
-          real_metric=float(real_metric)
-          expect_metric=float(expect_metric)
-
-          with assume: assert real_metric == approx(expect_metric, abs=3e-2),\
-                          "check eval_acc failed!   real eval_acc is: %s, \
-                            expect eval_acc is: %s" % (real_metric, expect_metric)
-
-      def test_ocr_rec_infer(self, use_gpu):
-          cmd='cd PaddleOCR; python tools/infer_rec.py -c %s  -o Global.use_gpu=%s Global.pretrained_model=./%s/best_accuracy Global.infer_img="./doc/imgs_words/en/word_1.png";' % (self.yaml, use_gpu, self.model)
-          if(platform.system() == "Windows"):
-               cmd=cmd.replace(';','&')
-          detection_result = subprocess.getstatusoutput(cmd)
-          exit_code = detection_result[0]
-          output = detection_result[1]
-          exit_check_fucntion(exit_code, output, 'infer')
-          metric=metricExtraction('result', output)
-          
-          rec_docs=metric.strip().split('\t')[0]
-          rec_scores=metric.strip().split('\t')[1]
-          rec_scores=float(rec_scores)
-
-          print('rec_docs:{}'.format(rec_docs))
-          print('rec_scores:{}'.format(rec_scores))
-
-          expect_rec_docs='joint'
-          expect_rec_scores=0.9999
-
-          with assume: assert rec_docs == expect_rec_docs,\
+        with assume: assert rec_docs == expect_rec_docs,\
                            "check rec_docs failed! real rec_docs is: %s,\
                             expect rec_docs is: %s" % (rec_docs, expect_rec_docs)
-          with assume: assert rec_scores == approx(expect_rec_scores, abs=1e-2),\
+        with assume: assert rec_scores == approx(expect_rec_scores, abs=1e-2),\
                           "check rec_scores failed!   real rec_scores is: %s, \
                             expect rec_scores is: %s" % (rec_scores, expect_rec_scores)
-          print("*************************************************************************")
+        print("*************************************************************************")
+     else:
+        pass
 
-
-      def test_ocr_export_model(self, use_gpu):
-          cmd='cd PaddleOCR; python tools/export_model.py -c %s -o Global.use_gpu=%s Global.pretrained_model=./%s/best_accuracy Global.save_inference_dir=./models_inference/%s;' % (self.yaml, use_gpu, self.model, self.model)
-          print(cmd)
-          if(platform.system() == "Windows"):
-               cmd=cmd.replace(';','&')
-          detection_result = subprocess.getstatusoutput(cmd)
-          exit_code = detection_result[0]
-          output = detection_result[1]
-          exit_check_fucntion(exit_code, output, 'export_model')
-
-      def test_ocr_rec_predict(self, use_gpu, use_tensorrt, enable_mkldnn):
-          model_config=yaml.load(open(os.path.join('PaddleOCR',self.yaml),'rb'), Loader=yaml.Loader)
-          rec_algorithm=model_config['Architecture']['algorithm']
-          print(rec_algorithm)
-          rec_image_shape=rec_image_shape_dict[rec_algorithm]
-          rec_char_dict_path=self.testcase_yml[self.model]['rec_char_dict_path']
-
-          print(rec_image_shape)
-          cmd='cd PaddleOCR; python tools/infer/predict_rec.py --image_dir="./doc/imgs_words_en/word_336.png" --rec_model_dir="./models_inference/"%s --rec_image_shape=%s --rec_algorithm=%s --rec_char_dict_path=%s --use_gpu=%s --use_tensorrt=%s --enable_mkldnn=%s;' % (self.model, rec_image_shape, rec_algorithm, rec_char_dict_path, use_gpu, use_tensorrt, enable_mkldnn)
-          if(platform.system() == "Windows"):
-               cmd=cmd.replace(';','&')
-          detection_result = subprocess.getstatusoutput(cmd)
-          print(cmd)
-          exit_code = detection_result[0]
-          output = detection_result[1]
-          exit_check_fucntion(exit_code, output, 'predict')
-          # acc
-          # metricExtraction('Predicts', output)
-
+def check_predict_metric(category, output):
+    if category=='rec':
           for line in output.split('\n'):
                   if 'Predicts of' in  line:
                       output_rec=line
@@ -219,6 +133,107 @@ class TestOcrModelFunction():
                           "check rec_scores failed!   real rec_scores is: %s, \
                             expect rec_scores is: %s" % (rec_scores, expect_rec_scores)
           print("*************************************************************************")
+    else:
+        pass
+
+class TestOcrModelFunction():
+      def __init__(self, model, yml, category): 
+         self.model=model
+         self.yaml=yml
+         self.category=category
+         self.testcase_yml=yaml.load(open('TestCase.yaml','rb'), Loader=yaml.Loader)
+         self.tar_name=os.path.splitext(os.path.basename(self.testcase_yml[self.model]['eval_pretrained_model']))[0]
+               
+      def test_ocr_train(self, use_gpu):
+          cmd='cd PaddleOCR; export CUDA_VISIBLE_DEVICES=0; sed -i s!data_lmdb_release/training!data_lmdb_release/validation!g %s; python -m paddle.distributed.launch --log_dir=log_%s  tools/train.py -c %s -o Global.use_gpu=%s Global.epoch_num=1 Global.save_epoch_step=1 Global.eval_batch_step=200 Global.print_batch_step=10 Global.save_model_dir=output/%s Train.loader.batch_size_per_card=10 Global.print_batch_step=1;' % (self.yaml,  self.model, self.yaml, use_gpu, self.model)
+          if(platform.system() == "Windows"):
+               cmd=cmd.replace(';','&')
+               cmd=cmd.replace('sed','%sed%')
+          if(platform.system() == "Darwin"):
+               cmd=cmd.replace('sed -i','sed -i ""')
+          print(cmd)
+          detection_result = subprocess.getstatusoutput(cmd)
+          exit_code = detection_result[0]
+          output = detection_result[1]
+          log_dir='PaddleOCR/log_'+self.model
+          exit_check_fucntion(exit_code, output, 'train', log_dir)
+      
+      def test_ocr_get_pretrained_model(self):
+          # cmd='cd PaddleOCR; wget %s; tar xf %s.tar; rm -rf *.tar; mv %s %s;' % (self.testcase_yml[self.model]['eval_pretrained_model'], self.tar_name, self.tar_name, self.model)
+          cmd=self.testcase_yml['cmd']['rec']['get_pretrained_model'] % (self.testcase_yml[self.model]['eval_pretrained_model'], self.tar_name, self.tar_name, self.model)
+          if(platform.system() == "Windows"):
+               cmd=cmd.replace(';','&')
+               cmd=cmd.replace('rm -rf', 'del')
+               cmd=cmd.replace('mv','ren')
+          print(cmd)
+          detection_result = subprocess.getstatusoutput(cmd)
+          exit_code = detection_result[0]
+          output = detection_result[1]
+          exit_check_fucntion(exit_code, output, 'eval')
+
+      def test_ocr_eval(self, use_gpu):
+          # cmd='cd PaddleOCR; python tools/eval.py -c %s  -o Global.use_gpu=%s Global.pretrained_model=./%s/best_accuracy' % (self.yaml, use_gpu, self.model)
+          cmd=self.testcase_yml['cmd'][self.category]['eval'] % (self.yaml, use_gpu, self.model) 
+          if(platform.system() == "Windows"):
+               cmd=cmd.replace(';','&')
+          print(cmd)
+          detection_result = subprocess.getstatusoutput(cmd)
+          exit_code = detection_result[0]
+          output = detection_result[1]
+          exit_check_fucntion(exit_code, output, 'eval')
+          real_metric=metricExtraction('acc', output)
+          expect_metric=self.testcase_yml[self.model]['eval_acc']
+          real_metric=float(real_metric)
+          expect_metric=float(expect_metric)
+
+          with assume: assert real_metric == approx(expect_metric, abs=3e-2),\
+                          "check eval_acc failed!   real eval_acc is: %s, \
+                            expect eval_acc is: %s" % (real_metric, expect_metric)
+
+      def test_ocr_rec_infer(self, use_gpu):
+          # cmd='cd PaddleOCR; python tools/infer_rec.py -c %s  -o Global.use_gpu=%s Global.pretrained_model=./%s/best_accuracy Global.infer_img="./doc/imgs_words/en/word_1.png";' % (self.yaml, use_gpu, self.model)
+          cmd=self.testcase_yml['cmd'][self.category]['infer'] % (self.yaml, use_gpu, self.model)
+          if(platform.system() == "Windows"):
+               cmd=cmd.replace(';','&')
+          detection_result = subprocess.getstatusoutput(cmd)
+          exit_code = detection_result[0]
+          output = detection_result[1]
+          exit_check_fucntion(exit_code, output, 'infer')
+
+          check_infer_metric(self.category, output)          
 
 
+      def test_ocr_export_model(self, use_gpu):
+          # cmd='cd PaddleOCR; python tools/export_model.py -c %s -o Global.use_gpu=%s Global.pretrained_model=./%s/best_accuracy Global.save_inference_dir=./models_inference/%s;' % (self.yaml, use_gpu, self.model, self.model)
+          cmd=self.testcase_yml['cmd'][self.category]['export_model'] % (self.yaml, use_gpu, self.model, self.model) 
+          print(cmd)
+          if(platform.system() == "Windows"):
+               cmd=cmd.replace(';','&')
+          detection_result = subprocess.getstatusoutput(cmd)
+          exit_code = detection_result[0]
+          output = detection_result[1]
+          exit_check_fucntion(exit_code, output, 'export_model')
 
+      def test_ocr_rec_predict(self, use_gpu, use_tensorrt, enable_mkldnn):
+          model_config=yaml.load(open(os.path.join('PaddleOCR',self.yaml),'rb'), Loader=yaml.Loader)
+          rec_algorithm=model_config['Architecture']['algorithm']
+          print(rec_algorithm)
+          if self.category=='rec':
+             rec_image_shape=rec_image_shape_dict[rec_algorithm]
+             rec_char_dict_path=self.testcase_yml[self.model]['rec_char_dict_path']
+
+             print(rec_image_shape)
+          # cmd='cd PaddleOCR; python tools/infer/predict_rec.py --image_dir="./doc/imgs_words_en/word_336.png" --rec_model_dir="./models_inference/"%s --rec_image_shape=%s --rec_algorithm=%s --rec_char_dict_path=%s --use_gpu=%s --use_tensorrt=%s --enable_mkldnn=%s;' % (self.model, rec_image_shape, rec_algorithm, rec_char_dict_path, use_gpu, use_tensorrt, enable_mkldnn)
+             cmd=self.testcase_yml['cmd'][self.category]['predict'] % (self.model, rec_image_shape, rec_algorithm, rec_char_dict_path, use_gpu, use_tensorrt, enable_mkldnn)
+          else:
+             cmd=self.testcase_yml['cmd'][self.category]['predict'] % (self.model, use_gpu, use_tensorrt, enable_mkldnn)
+          if(platform.system() == "Windows"):
+               cmd=cmd.replace(';','&')
+          detection_result = subprocess.getstatusoutput(cmd)
+          print(cmd)
+          exit_code = detection_result[0]
+          output = detection_result[1]
+          exit_check_fucntion(exit_code, output, 'predict')
+          # acc
+          # metricExtraction('Predicts', output)
+          check_predict_metric(self.category, output)
--- test_ocr_acc.py
+++ test_ocr_acc.py
@@ -1,4 +1,4 @@
-mport pytest
+import pytest
 import numpy as np
 import subprocess
 import re
@@ -15,11 +15,16 @@ from ModelsTestFramework import TestOcrModelFunction
 def get_model_list():
     import sys
     result = []
+    category=[]
     with open('models_list_test.yaml') as f:
       lines = f.readlines()
       for line in lines:
+         r = re.search('/(.*)/', line)
          result.append(line.strip('\n'))
-        # print(line)
+         print(r.group(1))
+         print(line)
+         print("************************")
+         category.append(r.group(1))
     return result
 
 
@@ -35,7 +40,10 @@ def setup_module():
 @pytest.mark.parametrize('yml_name', get_model_list())
 def test_rec_accuracy_get_pretrained_model(yml_name):
     model_name=os.path.splitext(os.path.basename(yml_name))[0]
-    model = TestOcrModelFunction(model=model_name, yml=yml_name)
+    r = re.search('/(.*)/', yml_name)
+    category=r.group(1)
+    print(category)
+    model = TestOcrModelFunction(model=model_name, yml=yml_name, category=category)
     model.test_ocr_get_pretrained_model()
 
 # @pytest.mark.skip
@@ -43,50 +51,72 @@ def test_rec_accuracy_get_pretrained_model(yml_name):
 @pytest.mark.parametrize("use_gpu", [True])
 def test_rec_accuracy_eval(yml_name, use_gpu):
     model_name=os.path.splitext(os.path.basename(yml_name))[0]
-    model = TestOcrModelFunction(model=model_name, yml=yml_name)
+    r = re.search('/(.*)/', yml_name)
+    category=r.group(1)
+    print(category)
+    model = TestOcrModelFunction(model=model_name, yml=yml_name, category=category)
     model.test_ocr_eval(use_gpu)
 
 @pytest.mark.parametrize('yml_name', get_model_list())
 @pytest.mark.parametrize("use_gpu", [True,False])
 def test_rec_accuracy_infer(yml_name, use_gpu):
     model_name=os.path.splitext(os.path.basename(yml_name))[0]
-    model = TestOcrModelFunction(model=model_name, yml=yml_name)
+    r = re.search('/(.*)/', yml_name)
+    category=r.group(1)
+    print(category)
+    model = TestOcrModelFunction(model=model_name, yml=yml_name, category=category)
     model.test_ocr_rec_infer(use_gpu)
 
 @pytest.mark.parametrize('yml_name', get_model_list())
 @pytest.mark.parametrize("use_gpu", [True,False])
 def test_rec_accuracy_export_model(yml_name, use_gpu):
     model_name=os.path.splitext(os.path.basename(yml_name))[0]
-    model = TestOcrModelFunction(model=model_name, yml=yml_name)
+    r = re.search('/(.*)/', yml_name)
+    category=r.group(1)
+    print(category)
+    model = TestOcrModelFunction(model=model_name, yml=yml_name, category=category)
     model.test_ocr_export_model(use_gpu)
 
 @pytest.mark.parametrize('yml_name', get_model_list())
 @pytest.mark.parametrize("enable_mkldnn", [True,False])
-def test_rec_accuracy_predict_mkl(yml_name, use_gpu, enable_mkldnn):
+def test_rec_accuracy_predict_mkl(yml_name, enable_mkldnn):
     model_name=os.path.splitext(os.path.basename(yml_name))[0]
-    model = TestOcrModelFunction(model=model_name, yml=yml_name)
+    r = re.search('/(.*)/', yml_name)
+    category=r.group(1)
+    print(category)
+    model = TestOcrModelFunction(model=model_name, yml=yml_name, category=category)
     model.test_ocr_rec_predict(False, 0, enable_mkldnn)
 
 @pytest.mark.parametrize('yml_name', get_model_list())
 @pytest.mark.parametrize("use_tensorrt", [True,False])
-def test_rec_accuracy_predict_trt(yml_name, use_gpu, use_tensorrt):
+def test_rec_accuracy_predict_trt(yml_name, use_tensorrt):
     model_name=os.path.splitext(os.path.basename(yml_name))[0]
-    model = TestOcrModelFunction(model=model_name, yml=yml_name)
+    r = re.search('/(.*)/', yml_name)
+    category=r.group(1)
+    print(category)
+    model = TestOcrModelFunction(model=model_name, yml=yml_name, category=category)
     model.test_ocr_rec_predict(True, use_tensorrt, 0)
 
-# @pytest.mark.skip
+@pytest.mark.skip
 @pytest.mark.parametrize('yml_name', get_model_list())
 @pytest.mark.parametrize("use_gpu", [True])
 def test_rec_funtion_train(yml_name, use_gpu):
     model_name=os.path.splitext(os.path.basename(yml_name))[0]
-    model = TestOcrModelFunction(model=model_name, yml=yml_name)
+    r = re.search('/(.*)/', yml_name)
+    category=r.group(1)
+    print(category)
+    model = TestOcrModelFunction(model=model_name, yml=yml_name, category=category)
     model.test_ocr_train(use_gpu)
 
+@pytest.mark.skip
 @pytest.mark.mac
 @pytest.mark.parametrize('yml_name', get_model_list())
 def test_rec_funtion_mac(yml_name):
     model_name=os.path.splitext(os.path.basename(yml_name))[0]
-    model = TestOcrModelFunction(model=model_name, yml=yml_name)
+    r = re.search('/(.*)/', yml_name)
+    category=r.group(1)
+    print(category)
+    model = TestOcrModelFunction(model=model_name, yml=yml_name, category=category)
 #    model.test_ocr_train(False)
 #    model.test_ocr_get_pretrained_model()
     model.test_ocr_eval(False)
