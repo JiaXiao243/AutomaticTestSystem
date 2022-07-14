6bba903ab774fe57a0f353decdbbf49ddfb711b0
jiaxiao243 <jiaxiao243@126.com>
2022-07-14 11:34:30 +0000

    update scripts

--- ModelsTestFramework.py
+++ ModelsTestFramework.py
@@ -34,7 +34,7 @@ class RepoInit():
          self.repo=repo
          print("This is Repo Init!")
          pid = os.getpid()
-         cmd='''ps aux| grep python | grep -v %s | awk '{print $2}'| xargs kill -9; rm -rf %s; git clone https://github.com/paddlepaddle/%s.git; cd %s; python -m pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple''' % (pid, self.repo, self.repo, self.repo)
+         cmd='''git clone -b dygraph https://github.com/paddlepaddle/%s.git; cd %s; python -m pip install -r requirements.txt''' % (self.repo, self.repo)
          repo_result=subprocess.getstatusoutput(cmd)
          exit_code=repo_result[0]
          output=repo_result[1]
@@ -172,7 +172,7 @@ class TestOcrModelFunction():
           elif self.category=='det':
              cmd=self.testcase_yml['cmd'][self.category]['train'] % (self.yaml, use_gpu, self.model)
           else:
-             pass
+             cmd=self.testcase_yml['cmd'][self.category]['train'] % (self.yaml, use_gpu, self.model)
 
           if(platform.system() == "Windows"):
                cmd=cmd.replace(';','&')
--- run_TestCase.sh
+++ run_TestCase.sh
@@ -1,4 +1,6 @@
-rm -rf PaddleOCR/rec_*
+# rm -rf PaddleOCR/rec_*
 python -m pip install -r requirements.txt
-export CUDA_VISIBLE_DEVICES=0,1
-python -m  pytest -sv test_ocr_acc.py --html=rec_report.html --capture=tee-sys
+# export CUDA_VISIBLE_DEVICES=0,1
+python -m pytest -sv test_ocr_acc_report.py  --alluredir=./result #--alluredir用于指定存储测试结果的路径)
+allure generate ./result/ -o ./report_test/ --clean
+# python -m  pytest -sv test_ocr_acc.py --html=rec_report.html --capture=tee-sys
