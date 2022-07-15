b582ad900e98b341b20d41592d7337ade318d6d1
JiaXiao243 <jiaxiao243@126.com>
2022-07-15 04:51:09 +0000

    update allure

--- run_TestCase.sh
+++ run_TestCase.sh
@@ -1,6 +1,10 @@
 # rm -rf PaddleOCR/rec_*
 # apt-get update
 # apt install -y openjdk-8-jdk
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
+
+
 python -m pip install -r requirements.txt
 # export CUDA_VISIBLE_DEVICES=0,1
 which allure
