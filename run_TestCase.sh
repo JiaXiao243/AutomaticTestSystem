41a31f11e1e6b8544b1379e83456b10762b549ae
jiaxiao243 <jiaxiao243@126.com>
2022-07-15 02:26:35 +0000

    updat test case

--- run_TestCase.sh
+++ run_TestCase.sh
@@ -1,7 +1,7 @@
 # rm -rf PaddleOCR/rec_*
 python -m pip install -r requirements.txt
 # export CUDA_VISIBLE_DEVICES=0,1
-python -m pytest -sv test_ocr_acc_report.py  --alluredir=./result #--alluredir用于指定存储测试结果的路径)
+python -m pytest -sv test_ocr_acc.py  --alluredir=./result #--alluredir用于指定存储测试结果的路径)
 ln -s /workspace/AutomaticTestSystem/allure/bin/allure /usr/bin/allure
 allure generate ./result/ -o ./report_test/ --clean
 # python -m  pytest -sv test_ocr_acc.py --html=rec_report.html --capture=tee-sys
