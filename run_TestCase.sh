55c5ea1151660df60f78700f84c067160609cab3
jiaxiao243 <jiaxiao243@126.com>
2022-07-15 02:21:58 +0000

    update test case

--- run_TestCase.sh
+++ run_TestCase.sh
@@ -2,5 +2,6 @@
 python -m pip install -r requirements.txt
 # export CUDA_VISIBLE_DEVICES=0,1
 python -m pytest -sv test_ocr_acc_report.py  --alluredir=./result #--alluredir用于指定存储测试结果的路径)
+ln -s /workspace/AutomaticTestSystem/allure/bin/allure /usr/bin/allure
 allure generate ./result/ -o ./report_test/ --clean
 # python -m  pytest -sv test_ocr_acc.py --html=rec_report.html --capture=tee-sys
