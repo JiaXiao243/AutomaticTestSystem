df5056d2b79b5fa092c0f1ca1d17d315fa0a2971
JiaXiao243 <jiaxiao243@126.com>
2022-07-20 05:27:36 +0000

    update environment

--- run_TestCase.sh
+++ run_TestCase.sh
@@ -11,6 +11,7 @@ rm -rf /usr/bin/allure
 ln -s /workspace/AutomaticTestSystem/allure/bin/allure /usr/bin/allure
 
 python -m pytest -sv test_ocr_acc.py  --alluredir=./result #--alluredir用于指定存储测试结果的路径)
-cp environment/environment.properties ./result 
+cp environment/environment.properties_linux ./result 
+mv ./result/environment/environment.properties_linux ./result/environment/environment.properties
 allure generate ./result/ -o ./report_test/ --clean
 # python -m  pytest -sv test_ocr_acc.py --html=rec_report.html --capture=tee-sys
