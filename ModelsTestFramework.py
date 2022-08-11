62b15cf6230c0c842d15074689bef8e990a7e553
JiaXiao243 <jiaxiao243@126.com>
2022-08-11 11:38:01 +0000

    fix bug

--- ModelsTestFramework.py
+++ ModelsTestFramework.py
@@ -466,7 +466,7 @@ class Test3DModelFunction():
           if(platform.system() == "Windows"):
                cmd=cmd.replace(';','&')
           print(cmd)
-          if (self.model=='smoke_dla34_no_dcn_kitti') or (self.model=='smoke_hrnet18_no_dcn_kitti') or (self.model=='centerpoint_pillars_016voxel_kitti') or (self.model=='centerpoint_pillars_02voxel_nuscenes_10sweep') 
+          if (self.model=='smoke_dla34_no_dcn_kitti') or (self.model=='smoke_hrnet18_no_dcn_kitti') or (self.model=='centerpoint_pillars_016voxel_kitti') or (self.model=='centerpoint_pillars_02voxel_nuscenes_10sweep'): 
                cmd='not supported for eval when bs >1' 
           detection_result = subprocess.getstatusoutput(cmd)
           exit_code = detection_result[0]
