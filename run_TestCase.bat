ef29c60890e3703edd9621fc3ac79db48a04ec99
JiaXiao243 <jiaxiao243@126.com>
2022-08-18 07:45:40 +0000

    update run scrupts

--- run_TestCase.bat
+++ run_TestCase.bat
@@ -2,7 +2,7 @@ set sed="C:\Program Files\Git\usr\bin\sed.exe"
 set CUDA_VISIBLE_DEVICES=0
 python -m pip install -r requirements.txt
 rem python -m  pytest -sv test_ocr_acc.py --html=rec_report.html --capture=tee-sys
-python -m pytest -sv test_ocr_acc.py  --alluredir=./result
+python -m pytest -sv %1  --alluredir=./result
 xcopy environment\environment.properties_win ./result /s /e /y /d
 cd result
 ren environment.properties_win environment.properties
