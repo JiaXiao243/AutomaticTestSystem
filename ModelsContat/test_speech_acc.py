e8c39c292975b433d4469d0b5d777e8f8e192b68
JiaXiao243 <jiaxiao243@126.com>
2022-08-30 08:19:34 +0000

    delete speech models

--- ModelsContat/test_speech_acc.py
+++ ModelsContat/test_speech_acc.py
@@ -41,32 +41,3 @@ def test_Speech_accuracy_cli(cmd):
     model = TestSpeechModelFunction()
     print(cmd)
     model.test_speech_cli(cmd)
-
-
-@allure.story('get_pretrained_model')
-@pytest.mark.parametrize('model_name', get_model_list())
-def test_Speech_accuracy_get_pretrained_model(model_name):
-    allure.dynamic.title(model_name+'_get_pretrained_model')
-    allure.dynamic.description('获取预训练模型')
-
-    model = TestSpeechModelFunction(model=model_name)
-    model.test_speech_get_pretrained_model()
-
-@allure.story('synthesize_e2e')
-@pytest.mark.parametrize('model_name', get_model_list())
-def test_speech_accuracy_synthesize_e2e(model_name):
-    allure.dynamic.title(model_name+'_synthesize_e2e')
-    allure.dynamic.description('模型评估')
-    
-    model = TestSpeechModelFunction(model=model_name)
-    model.test_speech_synthesize_e2e()
-
-
-@allure.story('train')
-@pytest.mark.parametrize('model_name', get_model_list())
-def test_speech_funtion_train(model_name):
-    allure.dynamic.title(model_name+'_train')
-    allure.dynamic.description('训练')
-
-    model = TestSpeechModelFunction(model=model_name)
-    model.test_speech_train()
