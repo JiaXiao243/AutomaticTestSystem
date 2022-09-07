a739e8b0335bef77fca0829d07f41c511a2a9ff2
JiaXiao243 <jiaxiao243@126.com>
2022-09-07 03:35:24 +0000

    fix bug

--- ModelsContat/run_requirement.sh
+++ ModelsContat/run_requirement.sh
@@ -1,12 +1,15 @@
-python -m pip install --upgrade parl opencv-python
-repo_list='PaddleClas PaddleOCR PaddleDetection PaddleSeg PaddleSpeech PaddleNLP PGL PaddleScience FastDeploy'
+# repo_list='PaddleClas PaddleOCR PaddleDetection PaddleSeg PaddleSpeech PaddleNLP PGL PaddleScience FastDeploy'
+set -x
+repo_list='PaddleClas PaddleOCR PaddleDetection PaddleSeg PaddleNLP PaddleSpeech'
 for repo in $repo_list
 do
 echo $repo
 git clone http://github.com/PaddlePaddle/$repo.git
 cd  $repo
+if [$repo!='PaddleSpeech'];then
 python -m pip install -r requirements.txt
+fi
+python -m pip install .
 cd ..
 done
-
 
