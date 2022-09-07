13354a16baae361d76fd7168736c9ac3cf3920d2
JiaXiao243 <jiaxiao243@126.com>
2022-09-07 06:44:49 +0000

    add print for init cmd

--- ModelsContat/RocmTestFramework.py
+++ ModelsContat/RocmTestFramework.py
@@ -79,6 +79,7 @@ class RepoInit():
          self.repo=repo
          print("This is Repo Init!")
          cmd='''git clone https://github.com/PaddlePaddle/%s.git --depth 1; cd %s; python -m pip install --upgrade pip; python -m pip install -r requirements.txt;''' % (self.repo, self.repo)
+         print(cmd)
          repo_result=subprocess.getstatusoutput(cmd)
          exit_code=repo_result[0]
          output=repo_result[1]
