788013d82ea4884f3a9c6989f7fdd2d520ec304c
JiaXiao243 <jiaxiao243@126.com>
2022-09-21 10:58:56 +0000

    updae for mac

--- run_TestCase_mac.sh
+++ run_TestCase_mac.sh
@@ -10,3 +10,9 @@ cp environment/environment.properties_mac ./result
 mv ./result/environment.properties_mac ./result/environment.properties
 allure generate ./result/ -o ./report_test/ --clean
 # python -m  pytest -sv test_ocr_acc.py --html=rec_report.html --capture=tee-sys
+set +x;
+export REPORT_SERVER="https://xly.bce.baidu.com/ipipe/ipipe-report"
+export REPORT_SERVER_USERNAME=$2
+export REPORT_SERVER_PASSWORD=$3
+curl -s ${REPORT_SERVER}/report/upload.sh | bash -s report_test $4 result
+echo "report uploaded"
