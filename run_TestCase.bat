23761f0167fe720e32188196f4606a06e47c95bb
JiaXiao243 <jiaxiao243@126.com>
2022-09-21 12:22:23 +0000

    update for mac

--- run_TestCase.bat
+++ run_TestCase.bat
@@ -3,13 +3,12 @@ set CUDA_VISIBLE_DEVICES=0
 python -m pip install -r requirements.txt
 rem python -m  pytest -sv test_ocr_acc.py --html=rec_report.html --capture=tee-sys
 python -m pytest -sv %1  --alluredir=./result
-xcopy environment\environment.properties_win ./result /s /e /y /d
-cd result
-ren environment.properties_win environment.properties
-cd ..
+rem xcopy environment\environment.properties_win ./result /s /e /y /d
+rem cd result
+rem ren environment.properties_win environment.properties
+rem cd ..
 allure generate ./result/ -o ./report_test/ --clean
 
-@echo off
 set REPORT_SERVER=https://xly.bce.baidu.com/ipipe/ipipe-report
 set REPORT_SERVER_USERNAME=%2
 set REPORT_SERVER_PASSWORD=%3
