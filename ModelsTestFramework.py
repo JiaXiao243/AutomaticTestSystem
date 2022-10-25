644f02f110da3561b922fa32430caabc58b8f409
JiaXiao243 <jiaxiao243@126.com>
2022-10-25 10:52:16 +0000

    fix bug

--- ModelsTestFramework.py
+++ ModelsTestFramework.py
@@ -162,7 +162,7 @@ class RepoDataset():
          assert exit_code == 0, "configure failed!   log information:%s" % output
          logging.info("configure dataset sucessfuly!" )
          if (sysstr == "Windows"):
-            cmd="mklink /d  pretrain_models F:\PaddleOCR\pretrain_models"
+            cmd="rd /s /q pretrain_models & mklink /d  pretrain_models F:\PaddleOCR\pretrain_models"
          else:
             cmd ='''cd PaddleOCR; rm -rf pretrain_models; wget -P ./pretrain_models/ https://paddleocr.bj.bcebos.com/pretrained/ResNet50_vd_ssld_pretrained.pdparams; wget -P pretrain_models https://paddle-qa.bj.bcebos.com/rocm/abinet_vl_pretrained.pdparams; wget -P pretrain_models https://paddleocr.bj.bcebos.com/dygraph_v2.1/en_det/ResNet50_dcn_asf_synthtext_pretrained.pdparams; wget https://paddleocr.bj.bcebos.com/contribution/rec_resnet_rfl_visual_train.tar; tar xf rec_resnet_rfl_visual_train.tar; mv rec_resnet_rfl_visual_train  rec_resnet_rfl_visual; mv rec_resnet_rfl_visual pretrain_models;'''
          cmd=platformAdapter(cmd)
